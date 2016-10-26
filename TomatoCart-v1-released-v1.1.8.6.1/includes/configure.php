7d8584d5d7cfa7e74c4adcb7fb2fd500465ec8f9
raccon _gMZ <fanygomez1894@gmail.com>
2016-10-25 21:59:25 -0600

    TomatoCart NOPOSWOW

---
+++ TomatoCart-v1-released-v1.1.8.6.1/includes/configure.php
@@ -0,0 +1,31 @@
+<?php
+  define('HTTP_SERVER', 'http://gorss.byethost16.com');
+  define('HTTPS_SERVER', 'http://gorss.byethost16.com');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', 'gorss.byethost16.com');
+  define('HTTPS_COOKIE_DOMAIN', 'gorss.byethost16.com');
+  define('HTTP_COOKIE_PATH', '/TOMATOCART/TomatoCart-v1-released-v1.1.8.6.1/');
+  define('HTTPS_COOKIE_PATH', '/TOMATOCART/TomatoCart-v1-released-v1.1.8.6.1/');
+  define('DIR_WS_HTTP_CATALOG', '/TOMATOCART/TomatoCart-v1-released-v1.1.8.6.1/');
+  define('DIR_WS_HTTPS_CATALOG', '/TOMATOCART/TomatoCart-v1-released-v1.1.8.6.1/');
+  define('DIR_WS_IMAGES', 'images/');
+
+  define('DIR_WS_DOWNLOAD_PUBLIC', 'pub/');
+  define('DIR_FS_CATALOG', '/home/vol14_5/byethost16.com/b16_19006226/htdocs/TOMATOCART/TomatoCart-v1-released-v1.1.8.6.1/');
+  define('DIR_FS_ADMIN', 'admin/');
+  define('DIR_FS_WORK', '/home/vol14_5/byethost16.com/b16_19006226/htdocs/TOMATOCART/TomatoCart-v1-released-v1.1.8.6.1/includes/work/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+  define('DIR_FS_BACKUP', '/home/vol14_5/byethost16.com/b16_19006226/htdocs/TOMATOCART/TomatoCart-v1-released-v1.1.8.6.1/' . DIR_FS_ADMIN . 'backups/');
+  define('DIR_FS_CACHE', DIR_FS_CATALOG . 'cache/');
+  define('DIR_FS_CACHE_ADMIN', DIR_FS_CACHE . DIR_FS_ADMIN);
+
+  define('DB_SERVER', 'sql212.byethost16.com');
+  define('DB_SERVER_USERNAME', 'b16_19006226');
+  define('DB_SERVER_PASSWORD', 'brabus.01@');
+  define('DB_DATABASE', 'b16_19006226_noposwow');
+  define('DB_DATABASE_CLASS', 'mysql');
+  define('DB_TABLE_PREFIX', 'toc_');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+?>
